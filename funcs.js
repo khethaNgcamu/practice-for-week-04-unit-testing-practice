5e7d328a33c7f439250eaadaf9aa314a564397b5
Smanga Ngcamu <ngcamukhetha@gamil.com>
2024-04-12 09:59:19 +0200

    unit testing practice completed

--- funcs.js
+++ funcs.js
@@ -1,13 +1,31 @@
 function isFive(num) {
   // Your code here
+  return num === 5 
 }
 
 function isOdd(number) {
   // Your code here
+  if(typeof number !== "number"){
+    throw new Error("Input is not a number.");
+  }
+
+  return number % 2 !== 0;
 }
 
 function myRange(min, max, step = 1) {
   // Your code here
+  if (typeof min !== "number" || typeof max !== "number" || typeof step !== "number") {
+    throw new Error("Inputs must be numbers.");
+  }
+  
+  if(min > max){
+    return [];
+  }
+const range = [];
+  for(let i = min; i <= max; i += step){
+    range.push(i);
+  }
+  return range;
 }
 
 
--- test/class-spec.js
+++ test/class-spec.js
@@ -6,29 +6,34 @@ const { Word } = require("../class");
 describe("Word", function () {
   describe("Word constructor function", function () {
     it('should have a "word" property', function () {
-      expect.fail("replace with your code");
+      const word = new Word("test");
+      expect(word).to.have.property("word");
     });
   
     it('should set the "word" property when a new word is created', function () {
-      expect.fail("replace with your code");
+      const word = new Word("example");
+      expect(word.word).to.equal("example");
     });
   });
 
   describe("removeVowels function", function () {
     it("should return a the word with all vowels removed", function () {
-      expect.fail("replace with your code");
+      const word = new Word("smanga");
+      expect(word.removeVowels()).to.equal("smng");
     });
   });
 
   describe("removeConsonants function", function () {
     it("should return the word with the consonants removed", function () {
-      expect.fail("replace with your code");
+      const word = new Word("smanga");
+      expect(word.removeConsonants()).to.equal("aa");
     });
   });
   
   describe("pigLatin function", function () {
     it("should return the word converted to pig latin", function () {
-      expect.fail("replace with your code");
+      const word = new Word("hello");
+      expect(word.pigLatin()).to.equal("ellohay");
     });
   });
 });
